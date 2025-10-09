83499f633dc09970cca0e23b2abb67e4628b960a
Makotosotoayam <rnafis98@gmail.com>
2025-10-09 07:59:23 +0700

    checking profile

--- database/migrations/2025_10_07_072153_barang.php
+++ database/migrations/2025_10_07_072153_barang.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('barang', function (Blueprint $table) {
             $table->id();
-            $table->string('nama_barang');
+            $table->string('name_barang');
             $table->integer('jumlah_barang');
             $table->timestamps();
         });
