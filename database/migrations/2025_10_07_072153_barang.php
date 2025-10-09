1bb131fe1cf031d87172db96d037b77a40a912c6
Makotosotoayam <rnafis98@gmail.com>
2025-10-09 08:50:56 +0700

    table add

--- database/migrations/2025_10_07_072153_barang.php
+++ database/migrations/2025_10_07_072153_barang.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name_barang');
             $table->integer('jumlah_barang');
+            $table->integer('harga_barang');
             $table->timestamps();
         });
     }
